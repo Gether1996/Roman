b59c663ada01dd314b386d42a6aba4507efff5d6
PatKredatus <pat.kredatus@gmail.com>
2023-11-13 21:13:10 +0100

    nxdfg

--- Roman/urls.py
+++ Roman/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from viewer.views import homepage
 
 urlpatterns = [
+    path('', homepage, name='homepage'),
     path('admin/', admin.site.urls),
 ]
---
+++ static/scripts.js
@@ -0,0 +1,81 @@
+function smoothScroll(targetId) {
+    var target = document.getElementById(targetId);
+    if (target) {
+      var targetPosition = target.offsetTop; // Get the target element's position
+      var startPosition = window.pageYOffset; // Get current position
+      var distance = targetPosition - startPosition;
+      var duration = 1000; // Set the duration of the scroll in milliseconds
+      let start = null;
+
+      // Function to perform the scrolling animation
+      function animation(currentTime) {
+        if (start === null) {
+          start = currentTime;
+        }
+        var timeElapsed = currentTime - start;
+        var run = ease(timeElapsed, startPosition, distance, duration);
+        window.scrollTo(0, run);
+        if (timeElapsed < duration) {
+          requestAnimationFrame(animation);
+        }
+      }
+
+      // Easing function for smooth scrolling
+      function ease(t, b, c, d) {
+        t /= d / 2;
+        if (t < 1) return c / 2 * t * t + b;
+        t--;
+        return -c / 2 * (t * (t - 2) - 1) + b;
+      }
+
+      requestAnimationFrame(animation);
+    }
+}
+
+function scrollToTop() {
+  const startPosition = window.pageYOffset; // Get current position
+  const distance = -startPosition; // Calculate the distance to scroll to the top
+  const duration = 1000; // Set the duration of the scroll in milliseconds
+  let start = null;
+
+  // Function to perform the scrolling animation
+  function animation(currentTime) {
+    if (start === null) {
+      start = currentTime;
+    }
+    const timeElapsed = currentTime - start;
+    const run = ease(timeElapsed, startPosition, distance, duration);
+    window.scrollTo(0, run);
+    if (timeElapsed < duration) {
+      requestAnimationFrame(animation);
+    }
+  }
+
+  // Easing function for smooth scrolling
+  function ease(t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return c / 2 * t * t + b;
+    t--;
+    return -c / 2 * (t * (t - 2) - 1) + b;
+  }
+
+  requestAnimationFrame(animation);
+}
+
+function initMap() {
+    var mapOptions = {
+        center: { lat: 49.209475, lng: 18.769308 },
+        zoom: 15, // Set the initial zoom level
+    };
+
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+    var marker = new google.maps.Marker({
+    position: { lat: 49.209475, lng: 18.769308 }, // Set the marker's position
+    map: map, // Set the map to display the marker
+    title: "My Marker", // Set a title for the marker (optional)
+});
+}
+
+// Call the initMap function when the page loads
+google.maps.event.addDomListener(window, "load", initMap);
