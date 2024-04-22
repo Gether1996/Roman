7fcc625a85a1e6a81f56094c0f2d582545267684
PatKredatus <pat.kredatus@gmail.com>
2024-04-22 12:44:47 +0200

    fasdf
    asdfa

--- Roman/settings.py
+++ Roman/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-sx5t9b&tpm9y0r+8(er1459^l2#tkplzq%2&#bu2wt_g-kuk7!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['masazevlcince.sk']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 DOMAINS = ['https://masazevlcince.sk']
 
