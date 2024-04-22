f9f5b7477dd9b983355e49a044cbe11d7d538fec
PatKredatus <pat.kredatus@gmail.com>
2024-04-22 12:42:18 +0200

    fasdf
    asdfasdf

--- Roman/settings.py
+++ Roman/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-sx5t9b&tpm9y0r+8(er1459^l2#tkplzq%2&#bu2wt_g-kuk7!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['masazevlcince.sk']
+
+DOMAINS = ['https://masazevlcince.sk']
+
+CSRF_TRUSTED_ORIGINS = DOMAINS
 
 
 # Application definition
